eaf11f57f708901235e929c80dc21847262a6dd4
unwriter <alejandra.alejandro@protonmail.com>
2018-06-04 10:31:54 -0400

    hello world

---
+++ app.js
@@ -0,0 +1,180 @@
+var global_locale = localStorage.getItem("locale")
+global_locale = (global_locale ? global_locale : "US");
+var global_currency = getCurrency(global_locale)
+var current_currency = currency_display(global_locale)
+// ADD API
+var add = function(addr) {
+  var snapshot = localStorage.getItem("snapshot")
+  if (snapshot) {
+    var last = JSON.parse(snapshot) 
+    balance(addr).then(function(item) {
+      last.items.push(item)
+      last.timestamp = Date.now()
+      localStorage.setItem("snapshot", JSON.stringify(last))
+      window.location.href = window.location.href.split("#")[0]
+    })
+  } else {
+    balance(addr).then(function(item) {
+      localStorage.setItem("snapshot", JSON.stringify({
+        items: [item],
+        timestamp: Date.now()
+      }))
+      window.location.href = window.location.href.split("#")[0]
+    })
+  }
+}
+if (window.location.hash.length > 1) {
+  // parse command
+  var paramstring = window.location.hash.substring(1)
+  var params = paramstring.split("=")
+  if (params.length == 2) {
+    if (params[0].toLowerCase() === 'add') {
+      var address_to_add = params[1];
+      add(address_to_add)
+    }
+  }
+}
+document.addEventListener("DOMContentLoaded", function(e) {
+  var tablesource   = document.getElementById("table-template").innerHTML;
+  var tabletemplate = Handlebars.compile(tablesource);
+  var fundsource   = document.getElementById("fund-template").innerHTML;
+  var fundtemplate = Handlebars.compile(fundsource);
+  var settingssource   = document.getElementById("settings-template").innerHTML;
+  var settingstemplate = Handlebars.compile(settingssource);
+  var snapshot = localStorage.getItem("snapshot")
+  document.querySelector("#settings").innerHTML = "<i class='fas fa-angle-down'></i> " + current_currency;
+  document.addEventListener("click", function(e) {
+    if (isClickable(e.target.closest("#api"))) {
+      document.querySelector(".api").classList.remove("hidden") 
+      e.preventDefault();
+      e.stopPropagation();
+    } else if (isClickable(e.target.closest("#settings"))) {
+      var html = settingstemplate({
+        currencies: currencies
+      })
+      document.querySelector(".settings .currency-selectors").innerHTML = html;
+      document.querySelector(".settings").classList.remove("hidden");
+      e.preventDefault();
+      e.stopPropagation();
+    } else if (isClickable(e.target.closest(".select-currency"))) { 
+      var selected = e.target.closest(".select-currency");
+      var locale = selected.dataset.locale;
+      localStorage.setItem("locale", locale)
+      e.preventDefault();
+      e.stopPropagation();
+      window.location.reload()
+    } else if (isClickable(e.target.closest("#add"))) {
+      var addr = document.querySelector(".address-input").value;
+      add(addr)
+      e.preventDefault();
+      e.stopPropagation();
+    } else if (isClickable(e.target.closest(".remove"))) {
+      var walletItem = e.target.closest(".wallet-item");
+      var remove_addr = bchaddr.toCashAddress(walletItem.dataset.addr);
+      var snapshot = localStorage.getItem("snapshot")
+      if (snapshot) {
+        var last = JSON.parse(snapshot) 
+        for(var i=0; i<last.items.length; i++) {
+          if (last.items[i].addr === remove_addr) {
+            last.items.splice(i, 1)
+            localStorage.setItem("snapshot", JSON.stringify({
+              items: last.items,
+              timestamp: Date.now()
+            }))
+            break;
+          }
+        }
+        window.location.reload()
+        e.preventDefault();
+        e.stopPropagation();
+      }
+    } else if (isClickable(e.target.closest(".wallet-link"))) {
+      var walletItem = e.target.closest(".wallet-item");
+      if (walletItem.classList.contains("selected")) {
+        walletItem.classList.remove("selected");
+        var infoItem = walletItem.querySelector(".info");
+        infoItem.innerHTML = "";
+      } else {
+        walletItem.classList.add("selected");
+        var infoItem = walletItem.querySelector(".info");
+        var addr = walletItem.dataset.addr;
+        var res = fundtemplate({
+          addr: addr,
+          cashAddr: bchaddr.toCashAddress(addr),
+          legacyAddr: bchaddr.toLegacyAddress(addr),
+          qr: qr(bchaddr.toCashAddress(addr))
+        })
+        infoItem.innerHTML = res;
+      }
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  })
+  if (snapshot) {
+    var last = JSON.parse(snapshot) 
+    rate(global_currency).then(function(r) {
+      Promise.all(last.items.map(function(item) {
+        return balance(item.addr)
+      }))
+      .then(function(results) {
+        var store = {
+          items: results
+        };
+        var total = results.reduce(function(acc, item) {
+          return acc + item.val
+        }, 0)
+        var res = tabletemplate({
+          converted: convert(total, r),
+          total: comma(total) + " satoshis (" + (total/100000000) + " BCH)",
+          items: results.map(function(result) {
+            // find the row by address from the last snapshot
+            // get the val and subtract from current
+            var delta = 0;
+            var converted_delta = 0;
+            var type = "";
+            for(var i=0; i<last.items.length; i++) {
+              if (last.items[i].addr === result.addr) {
+                delta = result.val - last.items[i].val; 
+                if (delta > 0) {
+                  converted_delta = (delta / 100000000 * r).toFixed(6);
+                  delta = "▲ " + delta;
+                  converted_delta = "▲ $" + converted_delta;
+                  type = 'up';
+                } else if (delta < 0) {
+                  converted_delta = (delta / 100000000 * r).toFixed(6);
+                  delta = "▼ " + (0-delta);
+                  converted_delta = "▼ $" + (0-converted_delta);
+                  type = 'down';
+                }
+                break;
+              }
+            }
+            return {
+              addr: bchaddr.toCashAddress(result.addr).split(":")[1],
+              val: comma(result.val),
+              type: type,
+              delta: (delta === 0 ? "" : "[" + delta + "]" ),
+              converted: convert(result.val, r),
+              converted_delta: (converted_delta === 0 ? "" : "[" + converted_delta + "]" ),
+            }
+          })
+        })
+        var d = jsondiffpatch.diff(last.items, store.items)
+        if (d) {
+          last = store;
+          last.timestamp = Date.now()
+          localStorage.setItem("snapshot", JSON.stringify(last))
+        }
+        document.body.querySelector(".balance").innerHTML = res;
+
+      })
+    })
+  } else {
+    var res = tabletemplate({
+      converted: convert(0, 1.0),
+      total: "0 BCH",
+      items: []
+    })
+    document.body.querySelector(".balance").innerHTML = res;
+  }
+})
---
+++ util.js
@@ -0,0 +1,50 @@
+var connection = datacash.connect('https://cashexplorer.bitcoin.com');
+var balance = function(addr) {
+  var legacyAddr = bchaddr.toLegacyAddress(addr)
+  var cashAddr = bchaddr.toCashAddress(addr)
+  return new Promise(function(resolve, reject) {
+    connection.getUnspentUtxos(legacyAddr, function(err, utxos) {
+      if (err) {
+        console.log("Error: ", err)
+      } else {
+        var sats = utxos.map(function(output) {
+          return output.satoshis
+        }).reduce(function(acc, sat) {
+          return sat + acc;
+        }, 0)
+        resolve({
+          val: sats,
+          addr: cashAddr
+        })
+      }
+    });
+  })
+}
+var rate = function(currency) {
+  return fetch("https://api.coinmarketcap.com/v2/ticker/1831/?convert="+currency)
+  .then(function(res) {
+    return res.json()
+  }).then(function(res) {
+    return res.data.quotes[currency].price;
+  })
+}
+var convert = function(balance, rate) {
+  var converted = balance * rate / 100000000;
+  return new Intl.NumberFormat(global_locale, { style: 'currency', currency: global_currency }).format(converted);
+}
+var comma = function(balance) {
+  return new Intl.NumberFormat().format(balance);
+}
+var qr = function(data) {
+  var q = qrcode(4, 'L');
+  q.addData(data);
+  q.make();
+  return q.createImgTag(4);
+}
+var isClickable = function(el) {
+  if (el && (el.nodeName === 'BUTTON' || el.nodeName === 'A')) {
+    return true;
+  } else {
+    return false;
+  }
+}
